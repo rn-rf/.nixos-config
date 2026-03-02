66d1b96638111effc1d0b326fa7ec857e273d03f
Aryan Rathod <aryanrkrathod@gmail.com>
2026-03-02 17:27:12 +0530

    replace symlinked dotfiles with real directories

--- home/rn.nix
+++ home/rn.nix
@@ -24,11 +24,12 @@
   programs.git.enable = true;
 
   fonts.fontconfig.enable = true;
+  
+  xdg.configFile."hypr".source = ../dotfiles/hypr;
+  xdg.configFile."waybar".source = ../dotfiles/waybar;
+  xdg.configFile."ghostty".source = ../dotfiles/ghostty;
+  xdg.configFile."tmux".source = ../dotfiles/tmux;
 
-  xdg.configFile."hypr".source = ../../dotfiles/hypr;
-  xdg.configFile."waybar".source = ../../dotfiles/waybar;
-  xdg.configFile."ghostty".source = ../../dotfiles/ghostty;
-  xdg.configFile."tmux".source = ../../dotfiles/tmux;
+  home.file.".bashrc".source = ../dotfiles/.bashrc;
 
-  home.file.".bashrc".source = ../../dotfiles/.bashrc;
 }
