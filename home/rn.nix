18cd0b077f796cf0df6f3d68d0b5d1e7756754ea
Aryan Rathod <aryanrkrathod@gmail.com>
2026-03-02 17:17:45 +0530

    fix relative paths

--- home/rn.nix
+++ home/rn.nix
@@ -25,10 +25,10 @@
 
   fonts.fontconfig.enable = true;
 
-  xdg.configFile."hypr".source = ../dotfiles/hypr;
-  xdg.configFile."waybar".source = ../dotfiles/waybar;
-  xdg.configFile."ghostty".source = ../dotfiles/ghostty;
-  xdg.configFile."tmux".source = ../dotfiles/tmux;
+  xdg.configFile."hypr".source = ../../dotfiles/hypr;
+  xdg.configFile."waybar".source = ../../dotfiles/waybar;
+  xdg.configFile."ghostty".source = ../../dotfiles/ghostty;
+  xdg.configFile."tmux".source = ../../dotfiles/tmux;
 
-  home.file.".bashrc".source = ../dotfiles/.bashrc;
+  home.file.".bashrc".source = ../../dotfiles/.bashrc;
 }
