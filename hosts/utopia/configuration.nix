f257c936c999ec9e2182069221d60d8a13f21994
Aryan Rathod <aryanrkrathod@gmail.com>
2026-03-14 11:40:37 +0530

    added tap and hold home row mod

--- hosts/utopia/configuration.nix
+++ hosts/utopia/configuration.nix
@@ -1,61 +1,71 @@
 {
-  config,
-  pkgs,
-  unstable,
-  ...
+    config,
+        pkgs,
+        unstable,
+        ...
 }:
 
 {
-  imports = [
-    ./hardware-configuration.nix
+    imports = [
+        ./hardware-configuration.nix
 
-    ../../modules/boot.nix
-    ../../modules/networking.nix
-    ../../modules/locale.nix
-    ../../modules/desktop.nix
-    ../../modules/audio.nix
-    ../../modules/bluetooth.nix
-    ../../modules/printing.nix
-    ../../modules/packages.nix
-  ];
-
-  networking.hostName = "Utopia";
-
-  users.users.rn = {
-    isNormalUser = true;
-    description = "Aryan Rathod";
-    extraGroups = [
-      "wheel"
-      "networkmanager"
-      "bluetooth"
-      "docker"
+            ../../modules/boot.nix
+            ../../modules/networking.nix
+            ../../modules/locale.nix
+            ../../modules/desktop.nix
+            ../../modules/audio.nix
+            ../../modules/bluetooth.nix
+            ../../modules/printing.nix
+            ../../modules/packages.nix
     ];
-  };
 
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
+    networking.hostName = "Utopia";
 
-  programs.hyprland.enable = true;
-  nixpkgs.config.allowUnfree = true;
-  system.stateVersion = "25.11";
-  programs.nix-ld.enable = true;
-  virtualisation.docker.enable = true;
-
-  services.xserver.videoDrivers = [ "nvidia" ];
-  hardware.graphics.enable = true;
-  hardware.nvidia = {
-    modesetting.enable = true;
-    powerManagement.enable = true;
-    powerManagement.finegrained = true;
-
-    open = false;
-    prime = {
-      offload.enable = true;
-      offload.enableOffloadCmd = true;
-      intelBusId = "PCI:0:2:0";
-      nvidiaBusId = "PCI:1:0:0";
+    users.users.rn = {
+        isNormalUser = true;
+        description = "Aryan Rathod";
+        extraGroups = [
+            "wheel"
+                "networkmanager"
+                "bluetooth"
+                "docker"
+        ];
     };
-  };
+
+    nix.settings.experimental-features = [
+        "nix-command"
+            "flakes"
+    ];
+
+    programs.hyprland.enable = true;
+    nixpkgs.config.allowUnfree = true;
+    system.stateVersion = "25.11";
+    programs.nix-ld.enable = true;
+    virtualisation.docker.enable = true;
+
+
+    services.kanata = {
+        enable = true;
+
+        keyboards.default = {
+            configFile = ../../dotfiles/kanata/kanata.kbd;
+        };
+    };
+
+
+    services.xserver.videoDrivers = [ "nvidia" ];
+    hardware.graphics.enable = true;
+    hardware.nvidia = {
+        modesetting.enable = true;
+        powerManagement.enable = true;
+        powerManagement.finegrained = true;
+
+        open = false;
+        prime = {
+            offload.enable = true;
+            offload.enableOffloadCmd = true;
+            intelBusId = "PCI:0:2:0";
+            nvidiaBusId = "PCI:1:0:0";
+        };
+    };
 }
